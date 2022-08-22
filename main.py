26edfcc94dec76c41dce844373c0ba04a8b8a82d
thooy <1493027794@qq.com>
2022-08-22 17:53:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ app_secret = os.environ["APP_SECRET"]
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
-
+/**
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+  return weather['weather'], math.floor(weather['temp'])**/
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
