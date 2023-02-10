1961ce9d061cd9beb37d96227704347328062244
thooy <1493027794@qq.com>
2023-02-10 11:51:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
-KEY = os.environ['KEY']
+key = os.environ['KEY']
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
@@ -20,7 +20,7 @@ template_id = os.environ["TEMPLATE_ID"]
 
 def get_weather():
 
-  url = "https://restapi.amap.com/v3/weather/weatherInfo?="+city+"&key="+KEY
+  url = "https://restapi.amap.com/v3/weather/weatherInfo?="+city+"&key="+key
   res = requests.get(url).json()
   weather = res['data']['list'][0]
   return weather['weather'], math.floor(weather['temp'])
