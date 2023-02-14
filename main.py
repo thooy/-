1a49a61303ff1bb01ceed78f25e58fa97236c105
thooy <1493027794@qq.com>
2023-02-14 08:55:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
-key_weather=os.environ['WEATHER']
+key_weather=os.environ['KEY_WEATHER']
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
